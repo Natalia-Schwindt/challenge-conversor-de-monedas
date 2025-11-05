dd4c3194e4ead2555a73b78137313d781bcdc1a7
Natalia-Schwindt <naty8014@gmail.com>
2025-11-05 19:47:38 -0300

    Estructura de paquetes y Gson agregado

---
+++ src/com/nataliaschwindt/conversor/principal/Main.java
@@ -0,0 +1,10 @@
+package com.nataliaschwindt.conversor.principal;
+
+import com.google.gson.Gson;
+
+public class Main {
+    public static void main(String[] args) {
+        Gson gson = new Gson();
+        System.out.println("Gson funciona!");
+    }
+}
